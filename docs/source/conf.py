0770053afa89f67f946267ae9eaa7c131ddc9d70
琤琮 <chengcong.mjm@autonavi.com>
2021-11-26 15:42:44 +0800

    doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,8 +65,7 @@ source_suffix = {
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 import sphinx_rtd_theme
 
 # html_theme = "sphinx_rtd_theme"
@@ -75,6 +74,6 @@ import sphinx_rtd_theme
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # language = "zh_CN"
